96f2d8ebaeaf26fda4b467e0acf8ba0e06e4994c
Devvrat Chaubal <84012086+devz-codes@users.noreply.github.com>
2021-10-19 13:04:25 +0530

    Second Commit

--- permutations.cpp
+++ permutations.cpp
@@ -1,45 +1,34 @@
 #include<iostream>
 #include<vector>
+
 using namespace std;
 
-int factorial(int num){
-  return num==0 ? 1: num*factorial(num-1);
-}
-
-void swap(int &i,int &j){
-  int c=i;
-  i=j;
-  j=c;
-}
-
-
-vector<vector<int>> permutation(vector<int> v){
+vector<vector<int>> permutations(vector<int> v,int len){
   vector<vector<int>> result={};
-  int len=v.size();
-
-  for(int i=0;i<factorial(len)/len;i++){
-    if(i%2==0){
-      for(int j=len-1;j>=1;j--){
-        swap(v[j],v[j-1]);
-        result.push_back(v);
+  if(len==2){
+  	result={{v[0],v[1]},{v[1],v[0]}};
+  }
+  else{
+    int num=v[len-1];
+    for(vector<int> i:permutations(v,len-1)){
+      for(int j=0;j<len;j++){
+        vector<int> dummy=i;
+        dummy.insert(dummy.begin()+j,num);
+        result.push_back(dummy);
       }
-      swap(v[len-1],v[len-2]);
-      result.push_back(v);
-    }
-    else{
-      for(int j=0;j<len-1;j++){
-        swap(v[j],v[j+1]);
-        result.push_back(v);
-      }
-      swap(v[0],v[1]);
-      result.push_back(v);
     }
   }
   return result;
 }
 
 int main(){
-  vector<int> num={1,2,3,4,5,6};
-  vector<vector<int>> p=permutation(num);
-  cout<<p.size();
+  vector<int> num={1,2,3,4,5,6,7};
+  vector<vector<int>> perm=permutations(num,7);
+  for(vector<int> j:perm){
+    for(int i:j){
+      cout<<i<<" ";
+    }
+    cout<<endl;
+  }
+  cout<<perm.size();
 }
